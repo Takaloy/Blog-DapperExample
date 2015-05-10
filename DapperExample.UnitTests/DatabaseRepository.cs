7f3cae466577e630d610ae73c6238fb18345901b
Daniel Low <takaloy@gmail.com>
2015-05-10 22:38:24 +0100

    initial commit

---
+++ DapperExample.UnitTests/DatabaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExample.UnitTests
+{
+    public class DatabaseRepository
+    {
+        public static void CreateDatabaseRepository()
+        {
+            SQLiteConnection.CreateFile(DbFile);
+        }
+
+        public static void TearDownDatabaseRepository()
+        {
+            File.Delete(DatabaseRepository.DbFile);
+        }
+
+        public static string DbFile
+        {
+            get { return Environment.CurrentDirectory + "\\SimpleDb.sqlite"; }
+        }
+
+        public static SQLiteConnection SimpleDbConnection()
+        {
+            return new SQLiteConnection("Data Source=" + DbFile);
+        }
+    }
+}
---
+++ DapperExample.UnitTests/SqlLiteDBSetup.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Dapper;
+using System.Threading.Tasks;
+
+namespace DapperExample.UnitTests
+{
+    [TestFixture]
+    public class SqlLiteDBSetup
+    {
+        [SetUp]
+        public void Setup()
+        {
+            DatabaseRepository.CreateDatabaseRepository();
+        }
+
+        [Test]
+        public void SimpleDataAccessTest()
+        {
+            var database = new DatabaseRepository();
+            var customerToInsert = new Customer
+            {
+                Id = 5,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            using (var connection = DatabaseRepository.SimpleDbConnection())
+            {
+                connection.Open();
+                CreateTable(connection);
+
+                CreateCustomer(connection, customerToInsert);
+            }            
+
+            using (var newconnection = DatabaseRepository.SimpleDbConnection())
+            {
+                var customerRetrieved = RetrieveCustomer(newconnection, customerToInsert.Id);
+
+                Assert.AreEqual(customerToInsert.FirstName, customerRetrieved.FirstName);
+                Assert.AreEqual(customerToInsert.LastName, customerRetrieved.LastName);
+            }
+        }
+
+        private void CreateTable(SQLiteConnection connection)
+        {
+            connection.Execute(
+                @"create table CUS_CUSTOMERS (
+                    Id integer primary key , 
+                    FirstName varchar(100) not null, 
+                    LastName varchar(100) not null)"
+                );            
+        }
+
+        private void CreateCustomer(SQLiteConnection connection, Customer customer)
+        {
+            
+
+            connection.Query<int>("insert into CUS_CUSTOMERS (ID, FirstName, LastName) values (@Id, @FirstName, @LastName);", customer);
+        }
+
+        private Customer RetrieveCustomer(SQLiteConnection connection, int id)
+        {
+            var result = connection.Query<Customer>(@"
+                select Id, FirstName, LastName from CUS_CUSTOMERS where Id = @Id
+                    ", new { Id = id }).FirstOrDefault();
+
+            return result;
+        }
+
+        [TearDown]
+        public void Tear()
+        {
+            DatabaseRepository.TearDownDatabaseRepository();
+        }
+    }
+}
